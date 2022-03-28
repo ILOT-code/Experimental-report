bce13134d2dcd48d90898b6a00ccd27b60d65b06
ILOT-code <a923865873@outlook.com>
2022-03-28 11:36:30 +0800

    22/3/28

---
+++ 4-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class stu{
+    public:
+    string name;
+    int a,b,c;
+};
+bool com( stu a,stu b){
+    if(a.a+a.b+a.c!=b.a+b.b+b.c) return a.a+a.b+a.c>b.a+b.b+b.c;
+    if(a.a!=b.a)                 return a.a>b.a;
+    if(a.b!=b.b)                 return a.b>b.b;
+    else                         return a.name<b.name;
+}
+int main(){
+    stu a[1024+5];
+    int N,k;
+    cin>>N;
+    for(int i=1;i<=N;++i){ cin>>a[i].name>>a[i].a>>a[i].b>>a[i].c;}
+    cin>>k;
+    sort(a+1,a+1+N,com);
+    cout<<a[k].name<<' '<<a[k].a+a[k].b+a[k].c;
+    return 0;
+      
+}
